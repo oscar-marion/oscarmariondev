45e4cea7d1619d16cc4b07db8c85ec618f188ae7
Oscar Marion <me@oscarmarion.dev>
2021-04-17 03:03:52 +0200

    fix: sticky footer offset on ios

--- components/template/Default.tsx
+++ components/template/Default.tsx
@@ -3,9 +3,9 @@ import { Container, DefaultHeader, DefaultFooter } from "components";
 
 export const Default: FC = ({ children }) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-full">
             <DefaultHeader />
-            <Container className="wrapper md:mt-6 flex-grow mb-12">
+            <Container className="wrapper md:mt-6 flex-auto mb-12">
                 <main id="wrapper">{children}</main>
             </Container>
             <DefaultFooter />
